719c7f3d32c050064f7f2a6f0e1e64c9b0e42aa8
egzix <159982449+ItsEgzix@users.noreply.github.com>
2024-12-09 11:47:08 +0800

    Update views.py

--- djmaschain/core/views.py
+++ djmaschain/core/views.py
@@ -32,9 +32,9 @@ def fetchdata(request):
 
         print(discription)
 
-        url = 'https://service-testnet.maschain.com/api/audit/audit'
-        client_id = 'b8350d8791d9bba95a36ce9dac93a1ac459836207afe55cc01158071f0eb9b47'
-        client_secret = 'sk_63190aebf1aa950ad33abcd9b1558d03cba39be2f629f0ff3adfb767fb410500'
+        url = 'API_URL'
+        client_id = 'id'
+        client_secret = 'id'
 
         headers = {
             'client_id': client_id,
@@ -43,8 +43,8 @@ def fetchdata(request):
 
         # Prepare the metadata and files for the API request
         metadata = {
-            'wallet_address': '0x50a15E05393DcAD29092c308D4128C27c7EE669f',
-            'contract_address': '0x8a9FfB105bC9645D5703DfE9b22279bB490c2a92',
+            'wallet_address': 'id',
+            'contract_address': 'id',
             'metadata[]': f'{car_Chasses_Number}, {manfuctureer}, {model}, {Year}, {Distance}, {color}, {time_stamp}, {discription}',
             'callback_url': 'https://service-testnet.maschain.com/'
         }
@@ -71,9 +71,9 @@ def search(request):
     if request.method == 'POST':
         search_chassis_number = request.POST.get('search_chassis_number')
 
-        url = f'https://service-testnet.maschain.com/api/audit/audit/{search_chassis_number}'
-        client_id = 'b8350d8791d9bba95a36ce9dac93a1ac459836207afe55cc01158071f0eb9b47'
-        client_secret = 'sk_63190aebf1aa950ad33abcd9b1558d03cba39be2f629f0ff3adfb767fb410500'
+        url = f'API_URL'
+        client_id = 'id'
+        client_secret = 'id'
 
         headers = {
             'client_id': client_id,
@@ -141,27 +141,4 @@ def fetch_data_by_chassis_number(chassis_number):
     return results if results else None
 
 
-# def fetch_data_by_chassis_number(chassis_number):
-#     # Path to the JSON file
-#     file_path = os.path.join(settings.BASE_DIR, 'data.json')
 
-#     # Read and parse the JSON file
-#     with open(file_path, 'r') as json_file:
-#         lines = json_file.readlines()
-
-#     # Process each JSON object in the file
-#     for line in lines:
-#         try:
-#             record = json.loads(line)
-#             metadata = record.get('result', {}).get('metadata', '')
-
-#             # Convert the metadata string to a list
-#             metadata_list = json.loads(metadata)
-#             # Check if the first item in the metadata list matches the chassis number
-#             if metadata_list[0].split(",")[0] == str(chassis_number):
-#                 return metadata_list[0].split(",")
-#         except json.JSONDecodeError:
-#             continue  # Skip lines that are not valid JSON
-
-#     # If no match is found, return None or an appropriate message
-#     return None
